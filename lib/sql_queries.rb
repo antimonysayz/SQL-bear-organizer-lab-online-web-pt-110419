22d2e03570830286d53817b517f7c57cb7398af7
Anthony Mai <anthony.mai530@gmail.com>
2020-01-24 03:14:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "select count(temperament) from bears group by temperament order by count(temperament) desc limit 1"
 end
 
 def selects_bear_that_killed_Tim
