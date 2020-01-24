8b0878266ee16f7ad12b0a0a5f917787250348a4
Anthony Mai <anthony.mai530@gmail.com>
2020-01-24 02:55:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, "Male", "Brown", "Relaxed", TRUE);
-  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "Female", "Brown", "Rowdy", TRUE);
-    insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 5 "Female", "Black", "Relaxed", TRUE);
-      insert into bears (name, age, gender, color, temperament, alive) values ("Seargent Brown", 6, "Male", "Black", "Grumpy", TRUE);
-        insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 7, "Female", "Black", "Relaxed", TRUE);
-          insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 6, "Male", "Brown", "Mean", TRUE);
-            insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 5, "Female", "Black", "Relaxed", TRUE);
-              insert into bears (age, gender, color, temperament, alive) values (5, "Male", "Brown", "Aggressive", TRUE);
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, "Male", "Brown", "Relaxed", true);
+  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "Female", "Brown", "Rowdy", true);
+    insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 5 "Female", "Black", "Relaxed", true);
+      insert into bears (name, age, gender, color, temperament, alive) values ("Seargent Brown", 6, "Male", "Black", "Grumpy", true);
+        insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 7, "Female", "Black", "Relaxed", true);
+          insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 6, "Male", "Brown", "Mean", true);
+            insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 5, "Female", "Black", "Relaxed", true);
+              insert into bears (age, gender, color, temperament, alive) values (5, "Male", "Brown", "Aggressive", true);
