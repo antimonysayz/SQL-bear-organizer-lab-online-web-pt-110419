c8ce3c689a5b0d42e984dcb7867782fbf8eb3402
Anthony Mai <anthony.mai530@gmail.com>
2020-01-24 02:53:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, "Male", "Brown", "Relaxed", TRUE)
-  insert into bears (name, age, gender, color, temperament, alive) values ()
-    insert into bears (name, age, gender, color, temperament, alive) values ()
-      insert into bears (name, age, gender, color, temperament, alive) values ()
-        insert into bears (name, age, gender, color, temperament, alive) values ()
-          insert into bears (name, age, gender, color, temperament, alive) values ()
-            insert into bears (name, age, gender, color, temperament, alive) values ()
-              insert into bears (name, age, gender, color, temperament, alive) values ()
-                insert into bears (name, age, gender, color, temperament, alive) values ()
+insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, "Male", "Brown", "Relaxed", TRUE);
+  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "Female", "Brown", "Relaxed", TRUE);
+    insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 5 "Female", "Black", "Relaxed", TRUE);
+      insert into bears (name, age, gender, color, temperament, alive) values ("Seargent Brown", 6, "Male", "Black", "Grumpy", TRUE);
+        insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 7, "Female", "Black", "Relaxed", TRUE);
+          insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 6, "Male", "Brown", "Mean", TRUE);
+            insert into bears (name, age, gender, color, temperament, alive) values ();
+              insert into bears (name, age, gender, color, temperament, alive) values ();
+                insert into bears (name, age, gender, color, temperament, alive) values ();
