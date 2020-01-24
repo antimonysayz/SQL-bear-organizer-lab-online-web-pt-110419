420ae98a2d97ddd41cbb59f68d8eaf41752bdc46
Anthony Mai <anthony.mai530@gmail.com>
2020-01-24 02:53:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 10, "Male", "Brown", "Relaxed", TRUE);
-  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "Female", "Brown", "Relaxed", TRUE);
+  insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 8, "Female", "Brown", "Rowdy", TRUE);
     insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 5 "Female", "Black", "Relaxed", TRUE);
       insert into bears (name, age, gender, color, temperament, alive) values ("Seargent Brown", 6, "Male", "Black", "Grumpy", TRUE);
         insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 7, "Female", "Black", "Relaxed", TRUE);
