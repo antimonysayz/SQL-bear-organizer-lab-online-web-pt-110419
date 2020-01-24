5160762128134e2b7371c5b4e3bdd5b3d9246606
Anthony Mai <anthony.mai530@gmail.com>
2020-01-24 03:09:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,15 +3,15 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "select name from bears order by name asc"
+  "select name from bears order by name asc;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "select name,age from bears where alive = 1 order by age asc"
+  "select name,age from bears where alive = 1 order by age asc;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "select name,age from bears where min(age)"
+  "select name,age from bears where min(age);"
 end
 
 def select_youngest_bear_and_returns_name_and_age
